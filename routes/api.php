bfb8075d24c203cb2a8249f1af353c33dfb4c0a8
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-05-23 17:25:26 +0900

    api接続

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route::get('/tasks',[TaskController::class, 'index']);
+// Route::post('/tasks',[TaskController::class, 'store'] );
+// Route::get('/tasks/{task}',[TaskController::class, 'show']);
+// Route::put('/tasks/{task}',[TaskController::class, 'update']);
+// Route::delete('/tasks/{task}',[TaskController::class, 'destroy']);
+
+
+/**
+ * laravel8から書き方が変わってる
+ * https://readouble.com/laravel/8.x/ja/upgrade.html
+ * https://qiita.com/tamakiiii/items/e71040173fa0a1fcad83
+ */
+Route::apiResources([
+    'tasks' => TaskController::class,
+]);
